f2a67b9297f3f61a0a9d0c9e02b8a955ce8108bd
thtTAMALtht <thtamal.cse@gmail.com>
2025-11-09 00:23:32 +0600

    login & register form design

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,82 @@
-import React from 'react';
+import { Link} from "react-router";
 
 const Login = () => {
+
   return (
-    <div>
-      login
+    <div className="flex justify-center mt-16">
+      <div className=" card bg-base-100 w-full border border-gray-200 max-w-2xl shrink-0 shadow-md">
+        <div className="card-body">
+          <h1 className="text-5xl font-bold text-primary text-center py-2">Login now!</h1>
+          <form>
+            <fieldset className="fieldset space-y-2">
+              <label className="label text-lg font-semibold">Email</label>
+              <input
+                type="email"
+                name="email"
+                className="outline w-full p-2 rounded-sm outline-gray-200 placeholder-gray-400"
+                placeholder="Email"
+              />
+              <label className="label text-lg font-semibold">Password</label>
+              <input
+                type="password"
+                name="password"
+                className="outline w-full p-2 rounded-sm outline-gray-200 placeholder-gray-400"
+                placeholder="Password"
+              />
+              <div>
+                <a className="link link-hover">Forgot password?</a>
+              </div>
+              <button className="btn btn-primary shadow-none text-white mt-4">
+                Login
+              </button>
+              <hr className="border-0 `h-[1px]` bg-gray-200" />
+              <button
+            
+                  type="button"
+                  className="btn bg-white text-black border-[#4388C9]"
+                >
+                  <svg
+                    aria-label="Google logo"
+                    width="16"
+                    height="16"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 512 512"
+                  >
+                    <g>
+                      <path d="m0 0H512V512H0" fill="#ffffff"></path>
+                      <path
+                        fill="#34a853"
+                        d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"
+                      ></path>
+                      <path
+                        fill="#4285f4"
+                        d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"
+                      ></path>
+                      <path
+                        fill="#fbbc02"
+                        d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"
+                      ></path>
+                      <path
+                        fill="#ea4335"
+                        d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"
+                      ></path>
+                    </g>
+                  </svg>
+                  Login with Google
+                </button>
+              <span className="mt-2 text-sm">
+                Dont have an account ?{" "}
+                <Link
+                  className="text-[#F76100] font-semibold"
+                  to="/auth/register"
+                >
+                  Register here!
+                </Link>
+              </span>
+            </fieldset>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,9 +1,102 @@
-import React from 'react';
+import { Link } from "react-router";
 
 const Register = () => {
+
+
   return (
-    <div>
-      register
+    <div className="flex justify-center mt-16">
+      <div className=" card bg-base-100 w-full border border-gray-200 max-w-2xl shrink-0 shadow-2xl">
+        <div className="card-body">
+          <h1 className="text-5xl font-bold text-center py-2 text-secondary">Register now!</h1>
+          <form>
+            <fieldset className="fieldset space-y-2">
+              <label className="label text-lg font-semibold">Name</label>
+              <input
+                type="text"
+                name="name"
+                className="outline w-full p-2 rounded-sm outline-gray-200 placeholder-gray-400"
+                placeholder="Name"
+              />
+
+              <label className="label text-lg font-semibold">Email</label>
+              <input
+                type="email"
+                name="email"
+                className="outline w-full p-2 rounded-sm outline-gray-200 placeholder-gray-400"
+                placeholder="Email"
+              />
+
+              <label className="label text-lg font-semibold">Photo URL</label>
+              <input
+                type="text"
+                name="photo"
+                className="outline w-full p-2 rounded-sm outline-gray-200 placeholder-gray-400"
+                placeholder="Photo URL"
+              />
+
+              <label className="label text-lg font-semibold">Password</label>
+              <input
+                type="password"
+                name="password"
+                className="outline w-full p-2 rounded-sm outline-gray-200 placeholder-gray-400"
+                placeholder="Password"
+              />
+              <button className="btn btn-secondary shadow-none mt-4 text-white">
+                Register
+              </button>
+              <div className="flex flex-col md:flex-row justify-between items-center">
+                <span className="text-sm">
+                  Already have an account ?{" "}
+                  <Link
+                    className="text-[#4388C9] font-semibold"
+                    to="/auth/login"
+                  >
+                    Login here!
+                  </Link>
+                </span>
+                <h3 className="text-sm text-[#F76100] hidden md:block">
+                  Alternative option ╰┈➤
+                </h3>
+                <button
+                
+                  type="button"
+                  className="btn mt-3 md:mt-0 w-full md:w-fit bg-white text-black border-[#4388C9]"
+                >
+                  <svg
+                    aria-label="Google logo"
+                    width="16"
+                    height="16"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 512 512"
+                  >
+                    <g>
+                      <path d="m0 0H512V512H0" fill="#fff"></path>
+                      <path
+                        fill="#34a853"
+                        d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"
+                      ></path>
+                      <path
+                        fill="#4285f4"
+                        d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"
+                      ></path>
+                      <path
+                        fill="#fbbc02"
+                        d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"
+                      ></path>
+                      <path
+                        fill="#ea4335"
+                        d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"
+                      ></path>
+                    </g>
+                  </svg>
+                  Login with Google
+                </button>
+              </div>
+              
+            </fieldset>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
